4cdf87f60b6f4d40407a1aed201912a19c5f334a
Meenakshi A.M <140526841+MeenakshiAM@users.noreply.github.com>
2025-02-26 11:24:24 +0000

    home page created

---
+++ my-portfolio/src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+    return(
+        <header className="header">
+            <nav className="nav-bar">
+                <span>PORTFOLIO</span>
+                <ul className="list">
+                    <li>home</li>
+                    <li>about me</li> 
+                    <li>project</li> 
+                    <li>skills</li>
+                    <li>contact me</li>              
+                </ul>
+            </nav>
+        </header>
+    );
+}
