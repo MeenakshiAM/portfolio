8868b68be60cbba8804c9c094bba7353188705b8
Meenakshi A.M <140526841+MeenakshiAM@users.noreply.github.com>
2025-03-08 09:08:22 +0000

    animation added

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -1,11 +1,39 @@
 import profileImg from "../assets/images/profile.jpg";
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const typedElement = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: [
+        "HELLO My dear lovely visitor",
+        "I am MEENAKSHI A M",
+        "I'm a Web Developer",
+        "Let's Create Something Amazing!",
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      backDelay: 1000,
+      startDelay: 500,
+      loop: true,
+    };
+
+    const typed = new Typed(typedElement.current, options);
+
+    return () => {
+      typed.destroy(); // Cleanup when component unmounts
+    };
+  }, []);
+
   return (
     <div className="home">
       <div className="home-content">
-        <h1>Welcome to My Portfolio</h1>
-        <p>This is my home page.</p>
+        <h1>Welcome!!!</h1>
+        <p>
+          <span ref={typedElement}></span> {/* Typed.js will type here */}.
+        </p>
       </div>
       <img src={profileImg} alt="profile" className="profile" />
     </div>
